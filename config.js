6221087502be12274698efc741387db48ca3ec79
lamlamqwer <lamlamqwer@gmail.com>
2021-10-16 14:43:57 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'HELLO CHỊ IU >3',
+    introDesc: `E CÓ 1 MÓN QUÀ NHỎ TẶNG CHỊ, CHỊ CÓ MUỐN BIẾT KHÔNG?.`,
+    btnIntro: 'OK',
+    title: 'HAPPY BIRTHDAY',
+    desc: 'chúc bé Linh 4  tuổi sinh nhật zui zẻ,lớn thêm 1 tuổi rồi bớt ngáo lại đi nha!(c có thích món quà này k)',
+    btnYes: 'Có, thích lúm cảm động muốn rớt nước mắt',
+    btnNo: 'không hề',
+    question:'Hãy nhập điều ước của bạn của đây ông tiên sẽ thực hiện nó!',
+    reasonPlaceholder: 'nhập điều ước',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'e là món quà lớn nhất mà ông trời đem đến cho chị rồi,c ko cần gì nữa',
+    mess: 'uhmmmmmmmm,nghe cũng sến súa ấy nhưng mà iu c',
+    messDesc: 'tiên said: đấy là câu trả lời của nó giờ hãy đến bên chàng trai ấy đui nào ta đi đây',
+    btnAccept: 'Ok lun',
+    messLink: 'https://www.messenger.com/t/100009504641647'
+}
